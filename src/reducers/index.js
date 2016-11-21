2ede11d34c488a1ab11203635948e7c57a3ccd9f
Craig Iturbe <citurbe@gmail.com>
2016-11-21 18:21:59 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,15 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+    return [...state, action.payload]
+      break;
+    case 'REMOVE_INGREDIENT':
+      return state.filter((item, index) => {
+        return index !== action.payload;
+      })
+      break;
+    default:
+      return state;
 
+  }
 }
